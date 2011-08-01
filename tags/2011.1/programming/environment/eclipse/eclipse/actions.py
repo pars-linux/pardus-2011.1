477e120c7b5db7fe6ff7d6c3637743e1dd43a378
gokcen.eraslan <gokcen.eraslan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-08-01 08:42:28 +0000

    Tag 2011.1 package repository.

---
+++ tags/2011.1/programming/environment/eclipse/eclipse/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2011 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = get.ARCH()
+
+def install():
+    pisitools.dodir("/opt")
+    pisitools.insinto("/opt", "eclipse")
+
+    # Make eclipse icon visible on start menu
+    # pisitools.insinto("/usr/share/pixmaps", "%s/opt/eclipse/plugins/org.eclipse.platform_3.6.2.v201102101200/eclipse48.png" % get.installDIR(), "eclipse.png")
